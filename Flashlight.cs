5564947696478b85290500487e77d24a8b568e4d
4domas <146350688+4domas@users.noreply.github.com>
2023-12-17 16:02:40 +0200

    kodas flaslight

---
+++ Flashlight.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public bool isOn;
+    public Light light;
+    public AudioSource source;
+    public float timer = 0;
+
+    public int battery1 = 10;
+
+    void Update()
+    {
+        if (isOn)
+        {
+            timer += Time.deltaTime;
+        }
+
+
+        if (timer < battery1)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Switch();
+            }
+            if (timer >= battery1)
+            {
+                light.intensity -= 1f;
+                timer = 0;
+            }
+        }
+
+        void Switch()
+        {
+            isOn = !isOn;
+            source.Play();
+        }        
+        light.enabled = isOn;
+    }
+}
